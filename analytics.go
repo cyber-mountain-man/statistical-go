d66982995920264a6e4b9efec264e20b794fcebb
Guillermo Morrison <morrison.guillermo@gmail.com>
2025-05-17 17:23:52 -0500

    Apply gofmt -s to analytics.go

--- analytics.go
+++ analytics.go
@@ -6,49 +6,25 @@ import (
 	"github.com/cyber-mountain-man/statistical-go/stat"
 )
 
-// --- Descriptive Stats ---
-func Mean(data []float64) float64 {
-	return stat.Mean(data)
-}
-
-func Variance(data []float64) float64 {
-	return stat.Variance(data)
-}
-
-func StdDev(data []float64) float64 {
-	return stat.StdDev(data)
-}
-
-func Median(data []float64) float64 {
-	return stat.Median(data)
-}
-
-func Mode(data []float64) float64 {
-	return stat.Mode(data)
-}
-
-func Range(data []float64) float64 {
-	return stat.Range(data)
-}
-
-func Min(data []float64) float64 {
-	return stat.Min(data)
-}
-
-func Max(data []float64) float64 {
-	return stat.Max(data)
-}
-
+// Descriptive Stats
+func Mean(data []float64) float64        { return stat.Mean(data) }
+func Variance(data []float64) float64    { return stat.Variance(data) }
+func StdDev(data []float64) float64      { return stat.StdDev(data) }
+func Median(data []float64) float64      { return stat.Median(data) }
+func Mode(data []float64) float64        { return stat.Mode(data) }
+func Range(data []float64) float64       { return stat.Range(data) }
+func Min(data []float64) float64         { return stat.Min(data) }
+func Max(data []float64) float64         { return stat.Max(data) }
 func Quartiles(data []float64) (float64, float64, float64) {
 	return stat.Quartiles(data)
 }
 
-// --- Probability ---
+// Probability
 func ConditionalProbability(probAandB, probB float64) float64 {
 	return probability.Conditional(probAandB, probB)
 }
 
-// --- Monte Carlo ---
+// Monte Carlo
 func EstimatePi(iterations int) float64 {
 	return montecarlo.EstimatePi(iterations)
 }
